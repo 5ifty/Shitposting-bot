de786473fa1360604fed800549fcb9dc2bbef78a
Harry <47954462+5ifty@users.noreply.github.com>
2019-06-26 20:11:31 +0100

    Forgot to upload, important to the bot, oops.

---
+++ utils/list.py
@@ -0,0 +1,134 @@
+ballresponse = [
+  'Yes', 'No', 'Take a wild guess...', 'Very doubtful',
+  'Sure', 'Without a doubt', 'Most likely', 'Might be possible',
+  "You'll be the judge", 'no... (╯°□°）╯︵ ┻━┻', 'no... baka',
+  'senpai, pls no ;-;'
+]
+
+
+age = [
+    '20 years old', '25 years old',
+    '30 years old', '40 years old', '50 years old',
+    '60 years old', '70 years old', '80 years old',
+    '90 years old'
+]
+
+country = [
+    'England', 'America', 'Holland', 'Spain', 'Argentina', 'France', 'Greece',
+    'Turkey', 'Nigeria', 'China', 'Japan', 'Brazil', 'Portugal', 'South-Africa',
+    'Canada', 'Finland', 'Sweeden', 'Norway'
+]
+
+sex = [
+    'Boy/Male', 'Girl/Woman', 'Transgender Male', 'Transgender Female',
+    'Apache Attack helicopter', 'Boy/Male', 'Girl/Woman', 'Boy/Male', 'Girl/Woman',
+    'Boy/Male', 'Girl/Woman', 'Boy/Male', 'Girl/Woman'
+]
+
+parentjob = [
+    'a doctor', 'a prostitue', 'a vet',
+    'an A-List Actor', ' a failed soundcloud rapper', 'a Nurse',
+    'their job is telling peoples futures', 'a programmer for a multi-million dollar company',
+    'they own a Billion Dollar Franchise', 'an instagram comedian',
+    'a singer and songwriter', 'a teacher',
+    'their job is caring for the the physically disabled', 'a pornstar',
+    'an artist', 'a chef', 'unknown'
+]
+parent = [
+    'Mother', 'father', 'Nan', 'Grandad', 'Uncle', 'Aunt'
+]
+
+siblings = [
+    'A Younger Brother', 'A Younger Sister', '2 Younger Sisters', '2 Younger Brothers',
+    'A twin sister', 'a twin brother'
+]
+
+gay = [
+    'Straight', 'Bisexual', 'Pansexual', 'Asexual', 'Homosexual', 'Lesbian', 'Bicurious'
+]
+
+school = [
+    'You are a pre-school drop out', 'You are a highschool drop out',
+    'You are a college drop out', 'You are a university drop out',
+    'You went to high-school and flunked every class', 'You graduated highschool',
+    'You went to college and studied Computer Science', 'You quit college to become a soundcloud rapper',
+    'You graduated college', 'You went to college and studied Art', 'You went to college and studied Maths',
+    'You went to college and studied Psyhcology', 'You went to college and studied English',
+    'You went to university to continue your dream of learning a new language',
+    'You went to university to graduate', 'You got a scholarship into your dream college',
+    'You went to a college to study Football', 'You studied Bio-Chemical Science at university',
+    'You dropped out of university'
+]
+
+license = [
+    'You passed your driving test', 'You failed your driving test', 'You drive a go kart',
+    'You can ride a bike', 'You can drive a tank'
+]
+
+
+job = [
+    'Your job is being a  doctor', 'Your job is prostitution', 'Your job is a vet',
+    'Your job is being an A-List Actor', 'Your Job is a failed soundcloud rapper', 'Your job is being a Nurse',
+    'Your job is telling peoples futures', 'Your job is being a programmer for a multi-million dollar company',
+    'You own a Billion Dollar Franchise', 'Your job is being an instagram comedian',
+    'Your job is being a singer and songwriter', 'Your job is being a teacher',
+    'Your job is caring for the the physically disabled', 'Your job is a pornstar',
+    'Your job is an artist', 'Your job is a chef'
+]
+
+money = [
+    'You make $1 a day', 'You make $10 a day', 'You make $100 a day', 'You make $500 a day',
+    'You make $1000 a day', 'You make $10000 a day', 'You make $5000 a day',
+    'You make $1 a month', 'You make $10 a month', 'You make $100 a month', 'You make $500 a month',
+    'You make $1000 a month', 'You make $10000 a month', 'You make $5000 a month', 'You make 100000 a month',
+    'You make $500 a month',
+    'You make $50000 a year', 'You make $100000 a year', 'You make $24000 a year', 'You make 150000 a year'
+]
+
+
+kids = [
+    'You have 1 child, a boy', 'You have 2 children, both boys',
+    'You have 1 child, a girl', 'You have 2 children, both girls',
+    'You have twins, both boys', 'You have twins, both girls',
+    'You do not have any children', 'You have twins, 1 boy 1 girl',
+    'You have 3 children, 2 boys 1 girl'
+]
+
+death = [
+    'You die at the age of 1', 'You die at the age of 5', 'You die at the age of 10',
+    'You die at the age of 15', 'You die at the age of 16', 'You die at the age of 17',
+    'You die at the age of 20', 'You die at the age of 22', 'You die at the age of 25',
+    'You die at the age of 27', 'You die at the age of 28', 'You die at the age of 30',
+    'You die at the age of 33', 'You die at the age of 35', 'You die at the age of 36',
+    'You die at the age of 39', 'You die at the age of 40', 'You die at the age of 41',
+    'You die at the age of 43', 'You die at the age of 45', 'You die at the age of 48',
+    'You die at the age of 50', 'You die at the age of 52', 'You die at the age of 55',
+    'You die at the age of 56', 'You die at the age of 59', 'You die at the age of 60',
+    'You die at the age of 63', 'You die at the age of 65', 'You die at the age of 68',
+    'You die at the age of 69', 'You die at the age of 70', 'You die at the age of 72',
+    'You die at the age of 74', 'You die at the age of 75', 'You die at the age of 77',
+    'You die at the age of 80', 'You die at the age of 81', 'You die at the age of 84',
+    'You die at the age of 85', 'You die at the age of 89', 'You die at the age of 90',
+    'You die at the age of 92', 'You die at the age of 93', 'You die at the age of 95',
+    'You die at the age of 97', 'You die at the age of 99', 'You die at the age of 100',
+    'You die at the age of 105', 'You die at the age of 110', 'You die at the age of 115',
+    'You die at the age of 120'
+]
+
+deathcause = [
+    'of aids', 'of food poisining', 'of parachute failure',
+    'of natural reasons', ' of you peacefully died in your sleep', ' of masturbating too much',
+    'of watching too much loli', ' of cancer', ' of complications with autism',
+    'of because you fell off a cliff', 'of slipping on a banana', ' of listening to cancerous music too much',
+    'of being too gay', 'of a drive by shooting', 'of a school shooting', 'of lethal injection',
+    'of snapping your peepee', 'of lead poisining', 'of allergies', 'of arsenic',
+    'of a heartattack', 'of Ebola', 'of toxic shock syndrome', 'of starvation',
+    'choking on a dildo'
+]
+
+fish = [
+    'There are no fishes to be caught, try again?', 'There are no fishes to be caught, try again?', 'There are no fishes to be caught, try again?',
+    'There are no fishes to be caught, try again?', 'There are no fishes to be caught, try again?', 'There are no fishes to be caught, try again?',
+    'You caught 🐟', 'You caught 🐠', 'You caught 🐡', 'You caught 🦈',
+    'There are no fishes to be caught, try again?', 'There are no fishes to be caught, try again?', 'There are no fishes to be caught, try again?'
+]
