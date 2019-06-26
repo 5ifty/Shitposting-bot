ef71952bf8008acb93e17d4342be5e12428b9219
Harry <47954462+5ifty@users.noreply.github.com>
2019-06-26 19:58:09 +0100

    Updated Markdowns to credit myself.

--- shitpostbot.py
+++ shitpostbot.py
@@ -18,8 +18,8 @@ import time
 
 description = '''A fun bot to shitpost your server!'''
 
-bot = commands.Bot(command_prefix='!!!', description=description)
-bot.remove_command('help')
+bot = commands.Bot(command_prefix='!!!', description=description)  # you can change the prefix & desc but please link it back to me<3
+bot.remove_command('help')   # removes default built in help command, delete this if you want the default one.
 token = '' # copy paste your token here to run the bot<3
 
 
@@ -27,8 +27,8 @@ token = '' # copy paste your token here to run the bot<3
 async def on_ready():
     print(f'Logging in as {bot.user.name}\n With the user ID {bot.user.id}')
     print(f'---Logged in---')
-    game = "With bad memes| !!!help "  # to avoid syntax errors (Cos im dumb), made it a variable.
-    await bot.change_presence(activity=discord.Game(type=3, name=game), status=discord.Status.dnd)
+    game = "With bad memes| !!!help "  # to avoid syntax errors (Cos im dumb), made it a variable. Change this at your own will, idrc.
+    await bot.change_presence(activity=discord.Game(type=3, name=game), status=discord.Status.dnd)  # again with this change at your own will , the status of the bot doesn't matter
 
 
 
@@ -398,8 +398,19 @@ async def about(ctx):
 
 
 
+# please don#t remove these 2 commands , I would love to be credited for making this bot!
+@bot.command()
+async def source(ctx):
+    await ctx.send(f"This is my source code! https://github.com/5ifty/Shitposting-bot\n and to get help join my discord server - https://discord.gg/jAaSeEx")
 
-# the help command, lol.
+
+@bot.command()
+async def owner(ctx):
+    await ctx.send(f"My owner is **5ifty#0777**, and you can join my home server here - https://discord.gg/jAaSeEx")
+
+
+
+# the help command
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title="Help/Info - To give this bot full functionality give it embed and message management permissions! Bot Prefix is !!!", color=ctx.me.top_role.colour, timestamp=datetime.utcnow())
@@ -407,7 +418,7 @@ async def help(ctx):
     embed.add_field(name="Voice/Text Commands", value=f"`reverse - Reverses your text, gnizama!\nsay - Make the Bot repeat what you say *Echoooo Echooo* (Manage message permissons needed)\ntts - Get a text to speech response!`", inline=False)
     embed.add_field(name="Games", value=f"`8ball - Get a response from the 8ball.\nfishing - Go fishing, maybe you'll catch one!\nlifegen - Makes up a random life for you, living my best life!`", inline=False)
     embed.add_field(name="User Inclusive", value=f"`blazeit - Spark up the joint with your friend!\nf - Can we get an F in the chat?\nhack - Fake hack someone *intensive typing*\nhowbig - Who said size doesn't matter?\nship - See how well 2 users ship toghether, it must be love<3\nvirgin - See how much of a virgin someone is!`", inline=False)
-    embed.add_field(name="Other", value=f"`about - Shows stats of the bot.\nhelp - Shows this message.\nping - Pong!`", inline=False)
+    embed.add_field(name="Other", value=f"`about - Shows stats of the bot.\nhelp - Shows this message.\nowner - See the owner of this bot and how to contact him<3\nping - Pong!\nsource - Shows the source code of this bot.`", inline=False)
     embed.set_footer(text="Shitpost bot help for Discord Hack Week", icon_url=ctx.bot.user.avatar_url)
     await ctx.send(embed=embed)
 
@@ -415,7 +426,9 @@ async def help(ctx):
 
 
 
-# This not nice part of code handles errors/ commands being used (for owner/Admin purposes only<3
+
+# This not nice part of code handles errors(for owner/Admin purposes only<3)
+# Will appear in your CMD.
 
 
 
